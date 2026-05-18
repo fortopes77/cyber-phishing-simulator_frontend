1260c8e8f206e60056a609b8a1944a650a0bd164
fortopes77 <azelia.clarkson1@gmail.com>
2026-05-18 15:16:43 +1000

    Fix merging issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,28 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/components/login/login.component';
-import { LoginComponent } from './auth/components/login/login.component';
 
 import { CommonModule, UpperCasePipe } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EffectsModule } from '@ngrx/effects';
 import { provideStore, StoreModule } from '@ngrx/store';
+import { ActionCardComponent } from 'src/app/shared/components/action-card/action-card.component';
+import { DataCardComponent } from 'src/app/shared/components/data-card/data-card.component';
+import { ActivityItemComponent } from './admin/components/activity-item/activity-item.component';
+import { ActivityListComponent } from './admin/components/activity-list/activity-list.component';
 import { AdminDashboardComponent } from './admin/components/admin-dashboard/admin-dashboard.component';
 import { AuthModule } from './auth/auth.module';
 import { AuthGuard } from './guards/auth.guard';
 import { UserDashboardComponent } from './learner/components/user-dashboard/user-dashboard.component';
-import { NavComponent } from './shared/components/navigation/nav.component';
-import { DataCardComponent } from 'src/app/shared/components/data-card/data-card.component';
 import { metaReducers } from './meta.reducer';
-import { ProgressRowComponent } from './shared/components/progress-row/progress-row.component';
 import { DashboardCardComponent } from './shared/components/dashboard-card/dashboard-card.component';
-import { ActivityItemComponent } from './admin/components/activity-item/activity-item.component';
-import { ActivityListComponent } from './admin/components/activity-list/activity-list.component';
-import { ActionCardComponent } from 'src/app/shared/components/action-card/action-card.component';
+import { NavComponent } from './shared/components/navigation/nav.component';
+import { ProgressRowComponent } from './shared/components/progress-row/progress-row.component';
 // import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
@@ -52,11 +49,6 @@ import { ActionCardComponent } from 'src/app/shared/components/action-card/actio
     ActivityItemComponent,
     ActivityListComponent,
     ActionCardComponent,
-  ],
-  providers: [
-    AuthGuard,
-    provideHttpClient(),
-    provideStore({}, { metaReducers }),
   ],
   providers: [
     AuthGuard,
